ea4f18d0cf151cf074ae29bee9db6da3b9e5c226
Matt Shenton <mshenton75@gmail.com>
2020-11-29 15:11:23 -0500

    Refactored PlayersList

--- api/players/index.js
+++ api/players/index.js
@@ -1,33 +1,15 @@
 require('../../config')
 const router = require('express').Router()
-const _ = require('lodash')
-const nba = require('nba')
+// const _ = require('lodash')
+// const nba = require('nba')
 const moment = require('moment')
+const PlayersList = require('../../lib/players_list')
+require('../../lib/players_list')
 
 router.get('/', async (req, res) => {
-  // TODO: refactor into seperate object
-  const ts = moment()
-  const schedule = await nba.data.schedule(ts.year())
-  const todaysGames = _.filter(schedule.league.standard, (data) => {
-    return data.startDateEastern === ts.format('YYYYMMDD')
-  })
-  const players = await createPlayerList(todaysGames)
-  const playerNames = _.map(players, 'player')
-  res.send(playerNames)
+  const date = moment()
+  const playersList = await new PlayersList(date).getList()
+  res.send(playersList)
 })
-
-async function createPlayerList (games) {
-  const promises = games.map(g => getPlayersForGame(g))
-  const players = await Promise.all(promises)
-  return _.flatten(players)
-}
-
-async function getPlayersForGame (game) {
-  const teams = [game.hTeam.teamId, game.vTeam.teamId]
-  const promises = teams.map(t => nba.stats.commonTeamRoster({ TeamID: t }))
-  const players = await Promise.all(promises)
-
-  return _.flatMap(players, 'commonTeamRoster')
-}
 
 module.exports = router
