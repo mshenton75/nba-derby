537601f4f592c0b8dc076ac0f36220786bbae4fa
Matt Shenton <mshenton75@gmail.com>
2020-11-29 15:14:27 -0500

    Removed reprecated module references

--- api/players/index.js
+++ api/players/index.js
@@ -1,7 +1,5 @@
 require('../../config')
 const router = require('express').Router()
-// const _ = require('lodash')
-// const nba = require('nba')
 const moment = require('moment')
 const PlayersList = require('../../lib/players_list')
 require('../../lib/players_list')
