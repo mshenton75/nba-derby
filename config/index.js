8ee6130aa046ea9ad98053e721d9c3b239680657
Matt Shenton <mshenton75@gmail.com>
2020-12-01 18:01:23 -0500

    Connected MongoDB Instance

--- config/index.js
+++ config/index.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 
 const logger = require('morgan')
 const app = express()
+const mongoose = require('mongoose')
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'))
@@ -13,5 +14,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
 
 module.exports = app
