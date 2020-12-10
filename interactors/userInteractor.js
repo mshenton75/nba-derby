8a5d6e0fe2617b79a373f5ccdb137c725eb90490
Matt Shenton <mshenton75@gmail.com>
2020-12-09 20:13:46 -0500

    Defined createUser in userInteractor

---
+++ interactors/userInteractor.js
@@ -0,0 +1,17 @@
+require('../config')
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+
+module.exports = {
+  createUser: async (params, fn) => {
+    const user = new User(params)
+    user.save(async (err, user) => {
+      console.log('hereh')
+      if (err) {
+        return fn({ status: 422, json: 'Failed to create user' })
+      }
+
+      fn({ status: 201, json: user })
+    })
+  }
+}
