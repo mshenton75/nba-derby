a9d9eccd77850f435b2e8d8f83f69046552158c5
Matt Shenton <mshenton75@gmail.com>
2020-12-10 16:25:57 -0500

    Removed cruft

--- api/v1/players.js
+++ api/v1/players.js
@@ -3,7 +3,6 @@ const router = require('express').Router()
 const moment = require('moment')
 const PlayersList = require.main.require('./lib/players_list')
 const _ = require('lodash')
-require.main.require('./lib/players_list')
 
 router.get('/', async (req, res) => {
   const date = moment(_.get(req.query, 'date'))
