8388800cb230c8219e73908549b46565c80dc193
Matt Shenton <mshenton75@gmail.com>
2020-12-11 16:41:40 -0500

    Updated homepage

--- api/index.js
+++ api/index.js
@@ -1,9 +1,8 @@
-var router = require('express').Router();
+const router = require('express').Router()
 
 router.route('/')
   .get(async function (req, res) {
-    console.log('here')
-    res.render('index', { title: 'Something!!' })
-  });
+    res.render('index')
+  })
 
-module.exports = router;
+module.exports = router
