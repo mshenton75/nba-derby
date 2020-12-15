a208c512d3b3c875ff7e030a0e6b0d66dff48775
Matt Shenton <mshenton75@gmail.com>
2020-12-14 22:26:53 -0500

    Serialize players from db collection

--- api/v1/players.js
+++ api/v1/players.js
@@ -3,11 +3,16 @@ const router = require('express').Router()
 const moment = require('moment')
 const PlayersList = require.main.require('./lib/players_list')
 const _ = require('lodash')
+const mongoose = require('mongoose')
+const ActivePlayer = mongoose.model('ActivePlayer')
 
 router.get('/', async (req, res) => {
   const date = moment(_.get(req.query, 'date'))
-  const playersList = await new PlayersList(date).getList()
-  res.send(playersList)
+  const playersDocument = await ActivePlayer.find({ date: date.format('YYYY-MM-DD') })
+  if (playersDocument.length === 0) {
+    return res.send('No players found for that date')
+  }
+  res.send(playersDocument[0].players)
 })
 
 module.exports = router
--- models/ActivePlayer.js
+++ models/ActivePlayer.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 const PlayersList = require.main.require('./lib/players_list')
 
 const ActivePlayer = new mongoose.Schema({
-  date: { type: Date, index: true },
+  date: { type: String, index: true },
   players: { type: Array }
 })
 
 ActivePlayer.statics.createPlayerListDocument = async function (date) {
   const players = await new PlayersList(date).getList()
-  this.create({ date: date, players: players }, (err, result) => {
+  this.create({ date: date.clone().format('YYYY-MM-DD'), players: players }, (err, result) => {
     if (err) return console.error(err)
   })
 }
