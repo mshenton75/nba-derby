d332897918227dd223bd2d35cf0409d31eff8048
Matt Shenton <mshenton75@gmail.com>
2020-12-26 20:52:12 -0500

    Changed time tasks run

--- tasks/index.js
+++ tasks/index.js
@@ -5,13 +5,13 @@ const schedule = require('node-schedule')
 const moment = require('moment')
 const _ = require('lodash')
 
-// run at 8:00 UTC
-schedule.scheduleJob('00 08 * * *', () => {
+// run at 9:00 UTC
+schedule.scheduleJob('00 09 * * *', () => {
   ActivePlayer.createPlayerListDocument(moment.utc())
 })
 
-// run at 7:45 UTC
-schedule.scheduleJob('45 07 * * *', async () => {
+// run at 8:30 UTC
+schedule.scheduleJob('30 08 * * *', async () => {
   const yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD')
   const games = await ActivePlayer.find({ date: yesterday })
   if (games) {
